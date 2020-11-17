6562a76ef8582dde99aeb1bcddb9e4f21ff2d901
GeorgeMC2610 <giorgosseimenis@gmail.com>
2020-11-17 22:20:00 +0200

    added some basic comments

--- DiceClicker.cs
+++ DiceClicker.cs
@@ -12,6 +12,7 @@ namespace csharp_vathomologoumeni_1
 {
     public partial class DiceClicker : Form
     {
+        //The array with the images, the difficulty, the random dice, score and time are all global variables, since we want them accessible in multiple functions.
         string[] Images = new string[6];
         short difficulty;
         short index = 0;
@@ -22,42 +23,52 @@ namespace csharp_vathomologoumeni_1
         public DiceClicker(short difficulty)
         {
             InitializeComponent();
+
+            //We get the difficulty from the previous form with this constructor.
             this.difficulty = difficulty;
         }
 
         private void DiceClicker_Load(object sender, EventArgs e)
         {
+            //Set the Image Locations as they're so we can pick a random image from the locations.
             Images[0] = "dice/dice1.png";
             Images[1] = "dice/dice2.png";
             Images[2] = "dice/dice3.png";
             Images[3] = "dice/dice4.png";
             Images[4] = "dice/dice5.png";
             Images[5] = "dice/dice6.png";
+
+            //Set the labels and timers with their starting values.
             label1.Text = "Time: 60";
             label2.Text = "Score: 0";
-            timer1.Enabled = true;
+            timer1.Enabled = timer2.Enabled = true;
 
+            //The location movement speed and picturebox size depend on the difficulty the user chose.
             switch (difficulty)
             {
                 case 1:
+                    //The easiest difficulty has one second movement speed and 400x400 image size
                     timer1.Interval = 1000;
                     pictureBox1.Width = pictureBox1.Height = 400;
                     label3.Text = "EASY";
                     label3.ForeColor = Color.Green;
                     break;
                 case 2:
+                    //The normal difficulty has 0.75 seconds movement speed and 300x300 image size
                     timer1.Interval = 750;
                     pictureBox1.Width = pictureBox1.Height = 300;
                     label3.Text = "NORMAL";
                     label3.ForeColor = Color.Orange;
                     break;
                 case 3:
+                    //The hard difficulty has half a second movement speed and 250x250 image size
                     timer1.Interval = 500;
                     pictureBox1.Width = pictureBox1.Height = 250;
                     label3.Text = "HARD";
                     label3.ForeColor = Color.Red;
                     break;
                 case 4:
+                    //The easiest difficulty has 0.3 seconds movement speed and 200x200 image size
                     timer1.Interval = 300;
                     pictureBox1.Width = pictureBox1.Height = 200;
                     label3.Text = "EXPERT";
@@ -68,10 +79,12 @@ namespace csharp_vathomologoumeni_1
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            //This changes randomly the dice
             Random random = new Random();
             index = (short) random.Next(0, 6);
             pictureBox1.ImageLocation = Images[index];
 
+            //the location also randomly changes. But we don't want it to clip out of the window. So we set the bounds accordingly
             int x = random.Next(Width - pictureBox1.Width);
             int y = random.Next(Height - pictureBox1.Height);
 
@@ -84,17 +97,19 @@ namespace csharp_vathomologoumeni_1
             label2.Text = "Score: " + score.ToString();
         }
 
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            time--;
+            if (time == 0)
+                EndGame();
+
+            label1.Text = "Time: " + time.ToString();
+        }
+
         private void EndGame()
         {
             new MainMenu().Show();
             Close();
-        }
-
-        private void timer2_Tick(object sender, EventArgs e)
-        {
-            time--;
-            if (time == 0) EndGame();
-            label1.Text = "Time: " + time.ToString();
         }
     }
 }
