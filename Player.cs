59a1b1e19302c83e285caded1cff6e89177f8312
GeorgeMC2610 <giorgosseimenis@gmail.com>
2020-11-19 18:19:30 +0200

    NOW IT WORKS. PART 1 HIGHSCORES ARE WORKING

--- Player.cs
+++ Player.cs
@@ -56,54 +56,57 @@ namespace csharp_vathomologoumeni_1
 
             //then we sort the arrays by descending score
             //the sorting algorithm (Bubble Sort) has to be hardcoded, since we want to sort multiple arrays.
-            for (int i = 1; i < points.Length; i++)
+            for (int i = 0; i < points.Length - 1; i++)
             {
-                for (int j = points.Length-1; j > i-1; j--)
+                for (int j = 0; j < points.Length - i - 1; j++)
                 {
-                    if (points[j] > points[j-1])
+                    if (points[j] < points[j + 1])
                     {
                         int temp0 = points[j];
-                        points[j] = points[j - 1];
-                        points[j - 1] = temp0;
+                        points[j] = points[j + 1];
+                        points[j + 1] = temp0;
 
                         string temp1 = name[j];
-                        name[j] = name[j - 1];
-                        name[j - 1] = temp1;
+                        name[j] = name[j + 1];
+                        name[j + 1] = temp1;
 
                         string temp2 = difficulty[j];
-                        difficulty[j] = difficulty[j - 1];
-                        difficulty[j - 1] = temp2;
+                        difficulty[j] = difficulty[j + 1];
+                        difficulty[j + 1] = temp2;
                     }
                 }
             }
 
-            //in order to display the data to the other form, we need to use this loop
-            for (int i = 0; i < points.Length; i++)
-            {
-                //first we create a variable for each possible difficulty we encounters
-                int easy_counter = 0, normal_counter = 0, hard_counter = 0, expert_counter = 0;
+            //first we create a variable for each possible difficulty we encounter
+            int easy_counter = 0;
+            int normal_counter = 0;
+            int hard_counter = 0;
+            int expert_counter = 0;
 
+            //in order to display the data to the other form, we need to use this loop
+            for (int i = 0; i < name.Length; i++)
+            {
                 //and then we display the first five of each difficulty.
                 //if there are less than five high score holders, the default value is three dashes ("---").
-                if (difficulty[i].Equals("EASY") && easy_counter <= 5)
+                if (difficulty[i].Equals("EASY") && easy_counter < 5)
                 {
                     MainMenu.EasyHI[easy_counter] = name[i] + ", " + points[i].ToString() + " points";
                     easy_counter++;
                 }
 
-                if (difficulty[i].Equals("NORMAL") && normal_counter <= 5)
+                else if (difficulty[i].Equals("NORMAL") && normal_counter < 5)
                 {
                     MainMenu.NormalHI[normal_counter] = name[i] + ", " + points[i].ToString() + " points";
                     normal_counter++;
                 }
 
-                if (difficulty[i].Equals("HARD") && easy_counter <= 5)
+                else if (difficulty[i].Equals("HARD") && hard_counter < 5)
                 {
                     MainMenu.HardHI[hard_counter] = name[i] + ", " + points[i].ToString() + " points";
                     hard_counter++;
                 }
 
-                if (difficulty[i].Equals("EXPERT") && easy_counter <= 5)
+                else if (difficulty[i].Equals("EXPERT") && expert_counter < 5)
                 {
                     MainMenu.ExpertHI[expert_counter] = name[i] + ", " + points[i].ToString() + " points";
                     expert_counter++;
